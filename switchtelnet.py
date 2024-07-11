637fcd93856ca3a5088571372e2eee1eda27d958
Resto Man <restoman71@outlook.com>
2024-07-11 15:52:43 -0700

    Add files via upload

---
+++ switchtelnet.py
@@ -0,0 +1,31 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.190.11"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"password\n")
+tn.write(b"conf t\n")
+tn.write(b"vlan 2\n")
+tn.write(b"name Python_VLAN_2\n")
+tn.write(b"vlan 3\n")
+tn.write(b"name Python_VLAN_3\n")
+tn.write(b"vlan 4\n")
+tn.write(b"name Python_VLAN_4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"name Python_VLAN_5\n")
+tn.write(b"show vlan brief\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
