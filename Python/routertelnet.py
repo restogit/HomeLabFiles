344016184075d1e8853b6512be7f8ea85c2008d9
Resto Man <restoman71@outlook.com>
2024-07-27 19:21:00 -0700

    Update and rename routertelnet.py to Python/routertelnet.py
    Removed hard coded enable password

--- Python/routertelnet.py
+++ Python/routertelnet.py
@@ -4,6 +4,7 @@ import telnetlib
 HOST = "ip-address-here"
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
+enablepass = getpass.getpass()
 
 tn = telnetlib.Telnet(HOST)
 
@@ -14,7 +15,10 @@ if password:
     tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"enable\n")
-tn.write(b"cisco\n")
+if enablepass:
+    tn.read_until(b"Password: ")
+    tn.write(enablepass.encode('ascii') + b"\n")
+    
 tn.write(b"conf t\n")
 tn.write(b"int loop 0\n")
 tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
